2190a86f1e1f0e55dd115eec79b9c49cb4a9ab62
Mukhammadyusuf Abdurakhimov <icoderx@yandex.com>
2023-08-10 14:22:50 -0700

    Last minute buid

--- app/math/components/sidebar.tsx
+++ app/math/components/sidebar.tsx
@@ -6,51 +6,13 @@ import { LayoutDashboard } from "lucide-react";
 import Link from "next/link";
 
 async function Sidebar() {
-  const navigation = [
-    {
-      id: 1,
-      question: "1 + 1 = ?",
-      answers: [
-        {
-          id: 1,
-          answer: "2",
-          isCorrect: true,
-        },
-        {
-          id: 2,
-          answer: "3",
-          isCorrect: false,
-        },
-        {
-          id: 3,
-          answer: "4",
-          isCorrect: false,
-        },
-      ],
-    },
-    {
-      id: 2,
-      question: "1^2+12/2",
-      answers: [
-        {
-          id: 1,
-          answer: "2",
-          isCorrect: false,
-        },
-        {
-          id: 2,
-          answer: "3",
-          isCorrect: false,
-        },
-        {
-          id: 3,
-          answer: "14/2",
-          isCorrect: true,
-        },
-      ],
-    },
-  ];
-
+  const navigation = await fetch("https://whit-rt.vercel.app/api/math")
+    .then((res) => {
+      return res.json();
+    })
+    .then((data) => {
+      return data.questions;
+    });
   return (
     <>
       <div className="flex grow flex-col gap-y-5 overflow-y-auto bg-white px-6 pb-4">
